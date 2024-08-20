f45ab2444b0ccadbeaec5644fc94b4ff24db0423
Robert Joseph Cox Jr <robert@studiorclv.com>
2024-08-19 21:30:36 -0700

    scaffolded the project

---
+++ spec/spec_helper.cr
@@ -0,0 +1,2 @@
+require "spec"
+require "../src/precious-metals-tracker"
---
+++ src/precious-metals-tracker.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Precious::Metals::Tracker`
+module Precious::Metals::Tracker
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
