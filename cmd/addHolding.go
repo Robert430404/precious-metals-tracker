b6a2cdfc50453fb315f6b50027d5882b01a1e840
Robert Joseph Cox Jr <robert@studiorclv.com>
2024-10-05 21:27:58 -0700

    integrated promptui into the add holding command, and moved the handler into another function declaration

--- cmd/addHolding.go
+++ cmd/addHolding.go
@@ -1,10 +1,36 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
+	"strconv"
 
 	"github.com/spf13/cobra"
+	"github.com/manifoldco/promptui"
 )
+
+func handleAddHolding(cmd *cobra.Command, args []string) {
+	validate := func(input string) error {
+		_, err := strconv.ParseFloat(input, 64)
+		if err != nil {
+			return errors.New("Invalid number")
+		}
+		return nil
+	}
+
+	prompt := promptui.Prompt{
+		Label:    "Number",
+		Validate: validate,
+	}
+
+	result, err := prompt.Run()
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	fmt.Printf("addHolding called %q\n", result)
+}
 
 var addHoldingCmd = &cobra.Command{
 	Use:   "addHolding",
@@ -20,9 +46,7 @@ It requests the following information:
 	- Type of holding
 
 This then stores it for use inside of the tool.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("addHolding called")
-	},
+	Run: handleAddHolding,
 }
 
 func init() {
