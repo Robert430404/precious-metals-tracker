8a469b0cb759234f7ab3f59f0233889d1e38ad9f
Robert Joseph Cox Jr <robert@studiorclv.com>
2024-10-06 20:02:24 -0700

    created holding struct for logic around building a holding

--- cmd/addHolding.go
+++ cmd/addHolding.go
@@ -3,74 +3,16 @@ package cmd
 import (
 	"fmt"
 
-	"github.com/manifoldco/promptui"
-	"github.com/robert430404/precious-metals-tracker/validations"
 	"github.com/robert430404/precious-metals-tracker/entities"
 	"github.com/spf13/cobra"
 )
 
-func PromptForValue(label string, validation promptui.ValidateFunc) (string, error) {
-	prompt := promptui.Prompt{
-		Label:    label,
-		Validate: validation,
-	}
-
-	result, err := prompt.Run()
-	if err != nil {
-		fmt.Printf("%q failed: %v", label, err)
-		return result, err
-	}
-
-	return result, nil
-}
-
-func PromptForType() (string, error) {
-	typeSelect := promptui.Select{
-		Label: "Holding Type",
-		Items: []string{entities.Silver, entities.Gold},
-	}
-
-	_, result, err := typeSelect.Run()
-	if err != nil {
-		fmt.Printf("Invalid type selected %v\n", err)
-		return result, err
-	}
-
-	return result, nil
-}
-
 func HandleAddHolding(cmd *cobra.Command, args []string) {
-	purchasePrice, err := PromptForValue("Purchase Price", validations.ValidatePrice)
-	if err != nil {
-		return
-	}
+	holding := entities.Holding{}
 
-	purchaseSource, err1 := PromptForValue("Purchase Source", validations.ValidateString)
-	if err1 != nil {
-		return
-	}
+	holding.Hydrate()
 
-	spotPrice, err2 := PromptForValue("Spot Price (at time of purchase)", validations.ValidatePrice)
-	if err2 != nil {
-		return
-	}
-
-	totalUnits, err3 := PromptForValue("How Many Units", validations.ValidateTotal)
-	if err3 != nil {
-		return
-	}
-
-	unitWeight, err4 := PromptForValue("Weight Of A Single Unit (in toz)", validations.ValidatePrice)
-	if err4 != nil {
-		return
-	}
-
-	holdingType, err5 := PromptForType()
-	if err5 != nil {
-		return
-	}
-
-	fmt.Printf("addHolding called %q, %q, %q, %q, %q, %q\n", purchasePrice, purchaseSource, spotPrice, totalUnits, unitWeight, holdingType)
+	fmt.Printf("addHolding called %q, %q, %q, %q, %q, %q\n", holding.Price, holding.Source, holding.PurchaseSpot, holding.Volume, holding.Weight, holding.Type)
 }
 
 var addHoldingCmd = &cobra.Command{
--- entities/holding.go
+++ entities/holding.go
@@ -1,5 +1,12 @@
 package entities
 
+import (
+	"fmt"
+
+	"github.com/manifoldco/promptui"
+	"github.com/robert430404/precious-metals-tracker/validations"
+)
+
 type HoldingType = string
 
 const (
@@ -8,5 +15,86 @@ const (
 )
 
 type Holding struct {
-	Type HoldingType
+	Price        string
+	Source       string
+	PurchaseSpot string
+	Volume       string
+	Weight       string
+	Type         HoldingType
+}
+
+func (self *Holding) Hydrate() error {
+	purchasePrice, err := self.PromptForValue("Purchase Price", validations.ValidatePrice)
+	if err != nil {
+		return err
+	}
+
+	self.Price = purchasePrice
+
+	purchaseSource, err1 := self.PromptForValue("Purchase Source", validations.ValidateString)
+	if err1 != nil {
+		return err1
+	}
+
+	self.Source = purchaseSource
+
+	spotPrice, err2 := self.PromptForValue("Spot Price (at time of purchase)", validations.ValidatePrice)
+	if err2 != nil {
+		return err2
+	}
+
+	self.PurchaseSpot = spotPrice
+
+	totalUnits, err3 := self.PromptForValue("How Many Units", validations.ValidateTotal)
+	if err3 != nil {
+		return err3
+	}
+
+	self.Volume = totalUnits
+
+	unitWeight, err4 := self.PromptForValue("Weight Of A Single Unit (in toz)", validations.ValidatePrice)
+	if err4 != nil {
+		return err4
+	}
+
+	self.Weight = unitWeight
+
+	holdingType, err5 := self.PromptForType()
+	if err5 != nil {
+		return err5
+	}
+
+	self.Type = holdingType
+
+	return nil
+}
+
+func (*Holding) PromptForValue(label string, validation promptui.ValidateFunc) (string, error) {
+	prompt := promptui.Prompt{
+		Label:    label,
+		Validate: validation,
+	}
+
+	result, err := prompt.Run()
+	if err != nil {
+		fmt.Printf("%q failed: %v", label, err)
+		return result, err
+	}
+
+	return result, nil
+}
+
+func (*Holding) PromptForType() (string, error) {
+	typeSelect := promptui.Select{
+		Label: "Holding Type",
+		Items: []string{Silver, Gold},
+	}
+
+	_, result, err := typeSelect.Run()
+	if err != nil {
+		fmt.Printf("Invalid type selected %v\n", err)
+		return result, err
+	}
+
+	return result, nil
 }
