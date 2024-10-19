18cbe1523b8c2cad4c0989282fa781392c49605e
Robert Joseph Cox Jr <robert@studiorclv.com>
2024-10-18 17:46:02 -0700

    setup the holding entity

--- db/entities/holding.go
+++ db/entities/holding.go
@@ -1,7 +1,17 @@
 package entities
 
-import "gorm.io/gorm"
+import (
+	"github.com/robert430404/precious-metals-tracker/models"
+	"gorm.io/gorm"
+)
 
 type Holding struct {
 	gorm.Model
+
+	Name              string
+	Source            string
+	PurchaseSpotPrice string
+	TotalUnits        string
+	UnitWeight        string
+	Type              models.HoldingType
 }
