72593abb8b2a83e2d10998c3644bd6fbdc9e5763
Robert Joseph Cox Jr <robert@studiorclv.com>
2024-10-20 22:26:35 -0700

    added the ability to delete a holding

---
+++ cmd/deleteHolding.go
@@ -0,0 +1,41 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/manifoldco/promptui"
+	"github.com/robert430404/precious-metals-tracker/db"
+	"github.com/robert430404/precious-metals-tracker/db/entities"
+	"github.com/robert430404/precious-metals-tracker/validations"
+	"github.com/spf13/cobra"
+)
+
+var deleteHoldingCmd = &cobra.Command{
+	Use:   "deleteHolding",
+	Short: "Deletes a holding",
+	Long:  `Deletes a holding from the system.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		prompt := promptui.Prompt{
+			Label:    "Holding ID",
+			Validate: validations.ValidateTotal,
+		}
+
+		result, err := prompt.Run()
+		if err != nil {
+			fmt.Printf("%q failed: %v", "Holding ID", err)
+			return
+		}
+
+		db := db.GetConnection()
+
+		db.Delete(&entities.Holding{}, result)
+		fmt.Println("deleteHolding called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(deleteHoldingCmd)
+}
