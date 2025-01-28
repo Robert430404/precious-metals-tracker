a2fdaa506bb623fe5c7c894645e813ef35811196
Robert Joseph Cox Jr <robert@studiorclv.com>
2025-01-27 19:09:43 -0800

    added unit testing for the json and table renderers

---
+++ renderers/jsonRenderer_test.go
@@ -0,0 +1,68 @@
+package renderers
+
+import (
+	"io"
+	"os"
+	"testing"
+
+	"github.com/robert430404/precious-metals-tracker/db/entities"
+	"github.com/robert430404/precious-metals-tracker/models"
+)
+
+func TestJsonRendererRenderHoldingList(t *testing.T) {
+	renderer := &JsonRenderer{}
+
+	mockData := []entities.Holding{
+		{
+			Name:              "name",
+			Source:            "source",
+			PurchaseSpotPrice: "spot-price",
+			TotalUnits:        "total-units",
+			UnitWeight:        "unit-weight",
+			Type:              models.Gold,
+		},
+	}
+
+	rescueStdout := os.Stdout
+	reader, writer, _ := os.Pipe()
+	os.Stdout = writer
+
+	renderer.RenderHoldingList(mockData)
+
+	writer.Close()
+	out, _ := io.ReadAll(reader)
+	os.Stdout = rescueStdout
+
+	expectedOutput := "[{\"ID\":0,\"CreatedAt\":\"0001-01-01T00:00:00Z\",\"UpdatedAt\":\"0001-01-01T00:00:00Z\",\"DeletedAt\":null,\"Name\":\"name\",\"Source\":\"source\",\"PurchaseSpotPrice\":\"spot-price\",\"TotalUnits\":\"total-units\",\"UnitWeight\":\"unit-weight\",\"Type\":\"Gold\"}]"
+	if string(out) != expectedOutput {
+		t.Fatalf("Output does not match: %s", out)
+	}
+}
+
+func TestJsonRendererRenderValueTable(t *testing.T) {
+	renderer := &JsonRenderer{}
+
+	mockData := [][]string{
+		{
+			"one",
+			"two",
+			"three",
+			"four",
+		},
+	}
+
+	rescueStdout := os.Stdout
+	reader, writer, _ := os.Pipe()
+	os.Stdout = writer
+
+	renderer.RenderValueTable(mockData)
+
+	writer.Close()
+	out, _ := io.ReadAll(reader)
+	os.Stdout = rescueStdout
+
+	expectedOutput := "[{\"type\": \"one\", \"currentValue\": \"two\", \"currentSpotPrice\": \"three\", \"totalHoldingWeight\": \"four\"}]"
+	if string(out) != expectedOutput {
+		t.Fatalf("Output does not match: %s", out)
+	}
+}
---
+++ renderers/tableRenderer_test.go
@@ -0,0 +1,107 @@
+package renderers
+
+import (
+	"io"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestRenderTable(t *testing.T) {
+	renderer := &TableRenderer{}
+
+	mockData := [][]string{
+		{
+			"one",
+			"two",
+			"three",
+			"four",
+		},
+	}
+
+	rescueStdout := os.Stdout
+	reader, writer, _ := os.Pipe()
+	os.Stdout = writer
+
+	renderer.RenderValueTable(mockData)
+
+	writer.Close()
+	out, _ := io.ReadAll(reader)
+	os.Stdout = rescueStdout
+
+	stringOut := string(out)
+
+	expectedToContain := "Metal Type"
+	if !strings.Contains(stringOut, expectedToContain) {
+		t.Fatalf("Output does not have: %s", expectedToContain)
+	}
+
+	expectedToContain = "Current Value"
+	if !strings.Contains(stringOut, expectedToContain) {
+		t.Fatalf("Output does not have: %s", expectedToContain)
+	}
+
+	expectedToContain = "Current Spot Price"
+	if !strings.Contains(stringOut, expectedToContain) {
+		t.Fatalf("Output does not have: %s", expectedToContain)
+	}
+
+	expectedToContain = "Total Holding Weight"
+	if !strings.Contains(stringOut, expectedToContain) {
+		t.Fatalf("Output does not have: %s", expectedToContain)
+	}
+
+	expectedToContain = "one"
+	if !strings.Contains(stringOut, expectedToContain) {
+		t.Fatalf("Output does not have: %s", expectedToContain)
+	}
+
+	expectedToContain = "two"
+	if !strings.Contains(stringOut, expectedToContain) {
+		t.Fatalf("Output does not have: %s", expectedToContain)
+	}
+
+	expectedToContain = "three"
+	if !strings.Contains(stringOut, expectedToContain) {
+		t.Fatalf("Output does not have: %s", expectedToContain)
+	}
+
+	expectedToContain = "four"
+	if !strings.Contains(stringOut, expectedToContain) {
+		t.Fatalf("Output does not have: %s", expectedToContain)
+	}
+
+	splitOut := strings.Split(stringOut, "\n")
+	splitExpected := []string{
+		"┌────────────────────────────────────────────────────────────────────────┐",
+		"│ Metal Type │ Current Value │ Current Spot Price │ Total Holding Weight │",
+		"├────────────────────────────────────────────────────────────────────────┤",
+		"│ one        │ two           │ three              │ four                 │",
+		"└────────────────────────────────────────────────────────────────────────┘",
+	}
+
+	firstLine := renderer.stripAnsiCodes(strings.Trim(splitOut[0], " "))
+	if firstLine != splitExpected[0] {
+		t.Fatalf("First line did not match: \n received: %s \n expected: %s", firstLine, splitExpected[0])
+	}
+
+	secondLine := renderer.stripAnsiCodes(strings.Trim(splitOut[1], " "))
+	if secondLine != splitExpected[1] {
+		t.Fatalf("Second line did not match: \n received: %s \n expected: %s", secondLine, splitExpected[1])
+	}
+
+	thirdLine := renderer.stripAnsiCodes(strings.Trim(splitOut[2], " "))
+	if thirdLine != splitExpected[2] {
+		t.Fatalf("Third line did not match: \n received: %s \n expected: %s", thirdLine, splitExpected[2])
+	}
+
+	fourthLine := renderer.stripAnsiCodes(strings.Trim(splitOut[3], " "))
+	if fourthLine != splitExpected[3] {
+		t.Fatalf("Fourth line did not match: \n received: %s \n expected: %s", fourthLine, splitExpected[3])
+	}
+
+	fifthLine := renderer.stripAnsiCodes(strings.Trim(splitOut[4], " "))
+	if fifthLine != splitExpected[4] {
+		t.Fatalf("Fifth line did not match: \n received: %s \n expected: %s", fifthLine, splitExpected[4])
+	}
+}
